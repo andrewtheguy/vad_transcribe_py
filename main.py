2cbcc00119c21a3cea44354d729463140f485438
Andrew Chen <andrew@jaanuu.com>
2024-12-07 13:58:38 -0800

    working

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 import torch
 from silero_vad import (load_silero_vad,
@@ -8,41 +9,31 @@ from silero_vad import (load_silero_vad,
                         VADIterator,
                         collect_chunks)
 
-import soundfile as sf
 
-from speech_detector.speech_detector import TARGET_SAMPLE_RATE, ffmpeg_get_16bit_pcm, pcm_s16le_to_float32
+from speech_detector.speech_detector import TARGET_SAMPLE_RATE, ffmpeg_get_16bit_pcm, pcm_s16le_to_float32, \
+    process_silero, record
 
 if __name__ == '__main__':
     argparse = argparse.ArgumentParser()
-    argparse.add_argument('--file', type=str, required=True)
+    argparse.add_argument('action', type=str, choices=['file','mic'])
+    argparse.add_argument('--file', type=str, required=False)
     args = argparse.parse_args()
 
-    with ffmpeg_get_16bit_pcm(args.file, target_sample_rate=TARGET_SAMPLE_RATE, ac=1) as stdout:
-        data = stdout.read()
-        audio = pcm_s16le_to_float32(data)
-
     model = load_silero_vad()
-    window_size_samples = 512 if TARGET_SAMPLE_RATE == 16000 else 256
 
-    #print(audio)
+    if args.action == 'file':
+        if not args.file:
+            print("Please provide a file path")
+            exit(1)
+        if not os.path.exists(args.file):
+            print(f"File {args.file} does not exist")
+            exit(1)
 
-    # Convert to PyTorch tensor and reshape to (1, num_samples)
-    # Silero typically expects a single-channel tensor with shape (1, samples)
-    audio_tensor = torch.from_numpy(audio)
-
-    speech_probs = []
-
-    wav = audio_tensor
-
-    for i in range(0, len(wav), window_size_samples):
-        chunk = wav[i: i + window_size_samples]
-        if len(chunk) < window_size_samples:
-            break
-        speech_prob = model(chunk, TARGET_SAMPLE_RATE).item()
-        speech_probs.append((i,speech_prob),)
-    model.reset_states()  # reset model states after each audio
-
-    # print indexes of speech_probs where probability is greater than 0.5
-    seconds = [i/TARGET_SAMPLE_RATE for i, prob in speech_probs if prob > 0.5]
-    print(seconds)
-    #sf.write('output.wav', audio, TARGET_SAMPLE_RATE)
+        with ffmpeg_get_16bit_pcm(args.file, target_sample_rate=TARGET_SAMPLE_RATE, ac=1) as stdout:
+            data = stdout.read()
+            audio = pcm_s16le_to_float32(data)
+        process_silero(model, audio)
+    elif args.action == 'mic':
+        audio = record()
+        process_silero(model, audio)
+        #sf.write('output.wav', audio, TARGET_SAMPLE_RATE)
--- speech_detector/speech_detector.py
+++ speech_detector/speech_detector.py
@@ -2,9 +2,13 @@ import subprocess
 from contextlib import contextmanager
 
 import numpy.typing as npt
+import torch
+from silero_vad import load_silero_vad
 
 TARGET_SAMPLE_RATE = 8000
 
+
+import sounddevice as sd
 
 # convert audio to 16 bit pcm with streaming output
 @contextmanager
@@ -93,6 +97,38 @@ def pcm_s16le_to_float32(pcm_bytes: bytes) -> npt.NDArray[np.float32]:
     audio_float32 = audio_int16.astype(np.float32) / (max_int16 + 1)
 
     return audio_float32
+
+def record():
+    fs = TARGET_SAMPLE_RATE
+    duration = 10  # seconds
+    myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype='float32')
+    sd.wait()  # Wait until recording is finished
+    return myrecording.squeeze()
+
+def process_silero(model,audio):
+    window_size_samples = 512 if TARGET_SAMPLE_RATE == 16000 else 256
+
+    # print(audio)
+
+    # Convert to PyTorch tensor and reshape to (1, num_samples)
+    # Silero typically expects a single-channel tensor with shape (1, samples)
+    audio_tensor = torch.from_numpy(audio)
+
+    speech_probs = []
+
+    wav = audio_tensor
+
+    for i in range(0, len(wav), window_size_samples):
+        chunk = wav[i: i + window_size_samples]
+        if len(chunk) < window_size_samples:
+            break
+        speech_prob = model(chunk, TARGET_SAMPLE_RATE).item()
+        speech_probs.append((i, speech_prob), )
+    model.reset_states()  # reset model states after each audio
+
+    # print indexes of speech_probs where probability is greater than 0.5
+    seconds = [i / TARGET_SAMPLE_RATE for i, prob in speech_probs if prob > 0.5]
+    print(seconds)
 
 class SpeechDetector:
     pass
